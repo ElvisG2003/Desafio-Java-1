9ff241b0fc62d5579211942ecacd8df47e4af4ec
ElvisG <elvisg.dev2003@gmail.com>
2025-03-01 17:36:41 -0300

    Avance primera funcion

--- app.js
+++ app.js
@@ -1,2 +1,13 @@
 let amigos = [];
+let nombre = "";
+
+function agregarAmigos(){
+    nombre = document.getElementById(`agregarAmigos`).value;
+    if (nombre.trim()=! "") { //Con .trim eliminamos los espacios vacios al principio y final del nombre, de tal forma que los espacios vacios no se suman a la lista//
+       amigos.push(nombre);
+       console.log(amigos); 
+    } else {
+        alert("Ingresa un nombre valido")
+    }
+}
 
